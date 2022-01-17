bb33d24ebd4e95ab158d57954577a3a7c12c0014
undefinednulla <undefinednulla@gmail.com>
2022-01-18 00:25:01 +0700

    reponsive complete beta ver1.0

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,7 +3,21 @@ $('.newest__slider').owlCarousel({
     margin: 10,
     nav: true,
     rewindNav: true,
-    navText: [$('.slider--next'), $('.slider--previous')],
+    navText: [$('.slider--previous'), $('.slider--next')],
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+})
+
+$('.companies').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    rewindNav: true,
+    navText: [$('.companies__slider--previous'), $('.companies__slider--next')],
     responsive: {
         0: {
             items: 1
@@ -18,3 +32,30 @@ const buttonNavication = document.getElementById('button--togle--mobile'),
 buttonNavication.addEventListener('click', () => {
     // elementLinks.
 })
+
+window.onload = function () {
+    const widthWindow = window.innerWidth
+    const companies = document.getElementById('companies__slider')
+
+    if (widthWindow > 500) {
+        companies.classList.remove('owl-carousel', 'owl-theme', 'owl-loaded', 'owl-drag')
+        companies.innerHTML = `<div class="companies__item">
+                <img src="./assets/images/companies/icon1.svg" alt="">
+            </div>
+            <div class="companies__item">
+                <img src="./assets/images/companies/icon2.svg" alt="">
+            </div>
+            <div class="companies__item">
+                <img src="./assets/images/companies/icon3.svg" alt="">
+            </div>
+            <div class="companies__item">
+                <img src="./assets/images/companies/icon4.svg" alt="">
+            </div>
+            <div class="companies__item">
+                <img src="./assets/images/companies/icon5.svg" alt="">
+            </div>
+            <div class="companies__item">
+                <img src="./assets/images/companies/icon6.svg" alt="">
+            </div>`
+    }
+};
