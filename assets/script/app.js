d0606d61aa92969b9d3c43af6c6684c22ea1e1ba
undefinednulla <undefinednulla@gmail.com>
2022-01-17 18:22:45 +0700

    complete desktop website

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,17 @@
-console.log("Hello Word")
+$('.newest__slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    rewindNav: true,
+    navText: [$('.slider--next'), $('.slider--previous')],
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+})
+
+// $('.newest__slider').find('.owl-nav').removeClass('disabled');
+// $('.newest__slider').on('changed.owl.carousel', function (event) {
+//     $(this).find('.owl-nav').removeClass('disabled');
+// });
